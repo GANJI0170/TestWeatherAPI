2e17636a2a1ab0527f330b6ca0e4794747e1321c
P.JooSeokHo <63225665+GANJI0170@users.noreply.github.com>
2023-11-20 16:24:30 +0900

    Weather Map API Test 날씨 한글화 함수 추가 날씨 상세정보 - 구름양, 습도, 바람속도 표시 추가

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,22 +2,30 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.Locale
 import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     private val API_KEY = "af8b16741ab74e53544c95208262593b"  // OpenWeatherMap에서 발급받은 API 키를 입력하세요
     private val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    private val iconBaseUrl = "https://openweathermap.org/img/w/"
     private lateinit var cityNameTextView: TextView
     private lateinit var temperatureTextView: TextView
     private lateinit var descriptionTextView: TextView
+    private lateinit var weatherIconView: ImageView
+    /*private lateinit var cloudinessView: TextView
+    private lateinit var humidityView: TextView
+    private lateinit var windSpeedView: TextView*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +34,12 @@ class MainActivity : AppCompatActivity() {
         cityNameTextView = findViewById(R.id.cityNameTextView)
         temperatureTextView = findViewById(R.id.temperatureTextView)
         descriptionTextView = findViewById(R.id.descriptionTextView)
-
+        weatherIconView = findViewById(R.id.weatherIconView)
+       /*
+        cloudinessView = findViewById(R.id.cloudinessView)
+        humidityView = findViewById(R.id.humidityView)
+        windSpeedView = findViewById(R.id.windSpeedView)
+*/
         val location = "Seoul,KR"  // 원하는 위치 입력
 
         val retrofit = Retrofit.Builder()
@@ -39,7 +52,10 @@ class MainActivity : AppCompatActivity() {
 
         val call = service.getWeather(location, API_KEY)
         call.enqueue(object : Callback<WeatherResponse> {
-            override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
+            override fun onResponse(
+                call: Call<WeatherResponse>,
+                response: Response<WeatherResponse>
+            ) {
                 if (response.isSuccessful) {
                     val weatherData = response.body()
                     Log.d("weather", "Success: $weatherData")
@@ -47,10 +63,26 @@ class MainActivity : AppCompatActivity() {
                     val temperatureCelsius = temperatureKelvin?.minus(273.15)?.roundToInt()
                     val description = weatherData?.weather?.get(0)?.description
                     val cityName = weatherData?.name
+                    val iconCode = weatherData?.weather?.getOrNull(0)?.icon
+                    val iconUrl = "$iconBaseUrl$iconCode.png"
+
+                    val koreanDescription = convertToKorean(description)
+                    val cloudiness = weatherData?.clouds?.all
+                    val humidity = weatherData?.main?.humidity
+                    val windSpeed = weatherData?.wind?.speed
                     runOnUiThread {
                         cityNameTextView.text = "도시 이름 : $cityName "
                         temperatureTextView.text = "현재 기온 : $temperatureCelsius°C"
-                        descriptionTextView.text = "현재 날씨 : $description"
+                        descriptionTextView.text = "현재 날씨 : $koreanDescription\n"  +
+                                "구름양: $cloudiness%\n" +
+                                "습도: $humidity%\n" +
+                                "바람 속도: $windSpeed m/s"
+
+
+                        Glide.with(this@MainActivity)
+                            .load(iconUrl)
+                            .into(weatherIconView)
+                        Log.d("weather", "Icon URL: $iconUrl")
                     }
                 } else {
                     Log.e("weather", "Error: ${response.code()} - ${response.message()}")
@@ -63,5 +95,19 @@ class MainActivity : AppCompatActivity() {
                 Log.e("weather", "Failure: ${t.message}")
             }
         })
+    }
+    // 날씨 상태를 한글로 변환하는 함수
+    private fun convertToKorean(description: String?): String {
+        return when (description?.toLowerCase(Locale.getDefault())) {
+            "clear sky" -> "맑음"
+            "few clouds" -> "구름 조금"
+            "scattered clouds" -> "구름이 조금 흩어짐"
+            "broken clouds", "overcast clouds" -> "구름이 많음"
+            "shower rain", "light rain", "moderate rain", "heavy intensity rain", "very heavy rain", "extreme rain" -> "비"
+            "thunderstorm", "thunderstorm with light rain", "thunderstorm with rain", "thunderstorm with heavy rain" -> "천둥번개"
+            "snow", "light snow", "sleet", "light shower sleet", "shower sleet", "light rain and snow", "rain and snow", "light shower snow", "shower snow", "heavy shower snow" -> "눈"
+            "mist", "smoke", "haze", "dust", "fog", "sand", "dust", "volcanic ash", "squalls", "tornado" -> "안개"
+            else -> description ?: ""
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/WeatherResponse.kt
+++ app/src/main/java/com/example/myapplication/WeatherResponse.kt
@@ -3,6 +3,8 @@ package com.example.myapplication
 data class WeatherResponse(
     val main: Main,
     val weather: List<Weather>,
+    val clouds: Clouds,
+    val wind: Wind,
     val name: String
 )
 
@@ -10,8 +12,14 @@ data class Main(
     val temp: Double,
     val humidity: Int
 )
-
+data class Clouds(
+    val all: Int
+)
+data class Wind(
+    val speed: Double
+)
 data class Weather(
     val main: String,
-    val description: String
+    val description: String,
+    val icon: String
 )
